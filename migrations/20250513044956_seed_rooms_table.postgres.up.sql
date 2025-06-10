20e9e53be7cb423ebf7a42c479edb0b79fb101f1
Akshata Kulkarni <akshatak@mkcl.org>
2025-06-10 10:34:17 +0530

    needful changes

---
+++ migrations/20250513044956_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2025-03-24 00:00:00.000','2025-03-24 00:00:00.000'),
+	 ('Major''s Suite','2025-03-26 00:00:00.000','2025-03-26 00:00:00.000');
